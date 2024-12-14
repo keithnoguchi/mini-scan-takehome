01b3cdd997fd11b56fc3c13eefe8df3764cbf346
Keith Noguchi <keith@noguchi.us>
2024-12-13 22:14:02 -0800

    cmd/processor: Initial commit of the processor service
    It's just for the bare minimum service with the docker
    compose build process.

---
+++ cmd/processor/main.go
@@ -0,0 +1,16 @@
+// A mini-scan-takehome scan processor.
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+	log.Println("Done")
+}
